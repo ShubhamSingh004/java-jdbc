b03ad3a3496d91889873714495002f0ffe3ecfda
Shubham <shubhamsinghmys@gmail.com>
2025-02-28 22:39:56 +0530

    Prepared Statements

--- Demo.java
+++ Demo.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -12,13 +13,36 @@ public class Demo {
 		Connection con = DriverManager.getConnection(url, "root", "mysql");
 		Statement st = con.createStatement();
 		
-		
 		ResultSet rs = st.executeQuery("select * from emp");
-		rs.next();
-		String name = rs.getString("name");
 		
-		System.out.println(name);
+		while(rs.next()) {
+			System.out.println(rs.getString("name") + ":" + rs.getInt("si"));
+		}
 		
+		
+		// update data on the sql table
+		
+		int count = st.executeUpdate("update emp set si = 4 where si = 3");
+		System.out.println("Row(s) affected: " + count);
+		
+		count = st.executeUpdate("insert into emp values('S3', 4)");
+		System.out.println("Row(s) affected: " + count);
+		
+		
+		
+		// prepare statements
+		PreparedStatement ps = con.prepareStatement("insert into emp values(?, ?)");
+		ps.setString(1, "s4");
+		ps.setInt(2, 7);
+		
+		ps.executeUpdate();
+		
+		
+		
+		
+		rs.close();
+		st.close();
+		con.close();
 				
 	}
 }
